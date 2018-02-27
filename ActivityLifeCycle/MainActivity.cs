79f716bc7a2297d0eba6512dc5995c062693e124
Jessica Anderson <Jessica Anderson@LAPTOP-HBQLSRM5>
2018-02-28 11:31:23 +1300

    Created First Page, Starting page - enter you name

--- ActivityLifeCycle/MainActivity.cs
+++ ActivityLifeCycle/MainActivity.cs
@@ -7,12 +7,44 @@ namespace ActivityLifeCycle
     [Activity(Label = "ActivityLifeCycle", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Toast.MakeText(this, "On Create", ToastLength.Short).Show();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Toast.MakeText(this, "On Start", ToastLength.Short).Show();
+        }
+
+        protected override void OnRestart()
+        {
+            base.OnStart();
+            Toast.MakeText(this, "On Restart", ToastLength.Short).Show();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnStart();
+            Toast.MakeText(this, "On Pause", ToastLength.Short).Show();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStart();
+            Toast.MakeText(this, "On Stop", ToastLength.Short).Show();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnStart();
+            Toast.MakeText(this, "On Destroy", ToastLength.Short).Show();
         }
     }
 }
